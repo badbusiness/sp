f58900c524c9ad552b68325443e5144e7450e800
badbusiness <martijn@creativehousing.nl>
2017-03-20 01:06:28 +0100

    Kleine updates

--- db/migrate/20170215232742_create_articles.rb
+++ db/migrate/20170215232742_create_articles.rb
@@ -1,7 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
-      t.integer :number
+      #t.integer :number
       t.string :description
       t.string :specification
       
