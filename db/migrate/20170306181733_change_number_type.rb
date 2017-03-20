33d38ba0aade3ea6699c51be3ad17e13e9a281ae
badbusiness <martijn@creativehousing.nl>
2017-03-20 01:09:32 +0100

    small shit

--- db/migrate/20170306181733_change_number_type.rb
+++ db/migrate/20170306181733_change_number_type.rb
@@ -1,5 +1,5 @@
 class ChangeNumberType < ActiveRecord::Migration[5.0]
   def change
-    change_column :articles, :number, :string
+    change_column :articles, :number, :string, 'integer USING CAST(column_name AS integer)'
   end
 end
