7ff42eedfb8c9ec89714a486a8c968a782c3d2bc
badbusiness <martijn@creativehousing.nl>
2017-03-20 01:12:51 +0100

    test

--- db/migrate/20170306181733_change_number_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNumberType < ActiveRecord::Migration[5.0]
-  def change
-    change_column :articles, :number, :string, 'integer USING CAST(column_name AS integer)'
-  end
-end
--- db/migrate/20170307013807_change_back_number_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBackNumberType < ActiveRecord::Migration[5.0]
-  def change
-    change_column :articles, :number, :integer
-  end
-end
