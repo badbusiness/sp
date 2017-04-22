3205a5240b153f7a9887fbad063467387bff8611
badbusiness <martijn@creativehousing.nl>
2017-04-22 13:49:35 +0200

    probeersel

--- db/migrate/20170419114736_add_fields_to_order_line.rb
+++ db/migrate/20170419114736_add_fields_to_order_line.rb
@@ -4,5 +4,18 @@ class AddFieldsToOrderLine < ActiveRecord::Migration[5.0]
     add_column :order_lines, :reduction, :decimal    
     add_reference :order_lines, :vat, index: true
     add_column :orders, :geleverd, :boolean, default: false
+    
+    
+    Vat.create([
+      {description: "geen", percentage: "0"},
+      {description: "laag", percentage: "0.06"},
+      {description: "hoog", percentage: "0.21"}
+    ])
+    
+    
+    remove_column :stock_articles, :vat, :float
+    add_column :stock_articles, :vat_id, :integer, default: 3
+    add_foreign_key :stock_articles, :vat
+    
   end
 end
