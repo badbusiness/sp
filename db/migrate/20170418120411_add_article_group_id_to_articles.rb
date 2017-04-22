22578c91ee582eb7c68cd43322296c5504e34a34
badbusiness <martijn@creativehousing.nl>
2017-04-22 13:11:25 +0200

    small fix

--- db/migrate/20170418120411_add_article_group_id_to_articles.rb
+++ db/migrate/20170418120411_add_article_group_id_to_articles.rb
@@ -1,5 +1,5 @@
 class AddArticleGroupIdToArticles < ActiveRecord::Migration[5.0]
   def change
-    add_reference :stock_articles, :article_group, foreign_key: true 
+    add_reference :stock_articles, :stock_article_group, foreign_key: true 
   end
 end
