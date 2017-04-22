37f455e1acd8180d0c1a944d4fac79ee797810df
badbusiness <martijn@creativehousing.nl>
2017-04-22 14:53:02 +0200

    and agaaaiiin....

--- app/models/Purchase/order_line.rb
+++ app/models/Purchase/order_line.rb
@@ -1,6 +1,6 @@
 class Purchase::OrderLine < OrderLine
   belongs_to :order
-  belongs_to :article, class_name: 'Stock::Article', foreign_key: 'article_id'
+  belongs_to :article, class_name: 'Stock::Article', foreign_key: 'stock_article_id'
   
   def price
     return self.article.purchase_price*amount
