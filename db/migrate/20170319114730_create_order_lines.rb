e9b3149f5230af41352fae401edaf8046a5dd088
badbusiness <martijn@creativehousing.nl>
2017-04-22 13:08:52 +0200

    kleine fix

--- db/migrate/20170319114730_create_order_lines.rb
+++ db/migrate/20170319114730_create_order_lines.rb
@@ -1,7 +1,7 @@
 class CreateOrderLines < ActiveRecord::Migration[5.0]
   def change
     create_table :order_lines do |t|
-      t.references :article, foreign_key: true
+      t.references :stock_article, foreign_key: true
       t.references :order, foreign_key: true
       t.string :type
       t.decimal :amount
