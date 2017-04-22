d7b2c838de40cf9265ef77d15b13ec39966d420b
badbusiness <martijn@creativehousing.nl>
2017-04-22 13:46:28 +0200

    small fix

--- db/migrate/20170419102026_create_vats.rb
+++ db/migrate/20170419102026_create_vats.rb
@@ -6,14 +6,18 @@ class CreateVats < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
-    remove_column :stock_articles, :vat, :float
-    add_column :stock_articles, :vat_id, :integer, default: 3
-    add_foreign_key :stock_articles, :vat_id
     
     Vat.create([
       {description: "geen", percentage: "0"},
       {description: "laag", percentage: "0.06"},
       {description: "hoog", percentage: "0.21"}
     ])
+    
+    
+    remove_column :stock_articles, :vat, :float
+    add_column :stock_articles, :vat_id, :integer, default: 3
+    add_foreign_key :stock_articles, :vat
+    
+    
   end
 end
